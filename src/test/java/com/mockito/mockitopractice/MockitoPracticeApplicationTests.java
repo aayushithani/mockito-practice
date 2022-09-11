681ecef9ccbfb88160a1edf1aeca629e0fa96fdf
aayushi_thani <aayushithani27@gmail.com>
2022-09-11 17:41:03 +0530

    project created

---
+++ src/test/java/com/mockito/mockitopractice/MockitoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockito.mockitopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
